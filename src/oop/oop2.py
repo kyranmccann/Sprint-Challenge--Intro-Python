2577092b95da82a7e793223418bbaaf6a7520da1
kyranmccann <kyran.mccann@gmail.com>
2019-02-15 09:15:45 -0800

    update oop2

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -20,8 +20,8 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 class Motorcycle(GroundVehicle):
-    def __init__(self, num_wheels=2):
-        self.num_wheels = num_wheels
+    def __init__(self):
+        super().__init__(2)
 
     def drive(self):
         return "BRAAAP!!"
